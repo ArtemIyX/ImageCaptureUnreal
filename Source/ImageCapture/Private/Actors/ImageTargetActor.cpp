dd59c75737bd1138dd21113c1fcf3dca9a53aca1
Wellsaik <artem.podorozhko@outlook.com>
2024-09-05 11:14:45 +0300

    Actor and component (basic)

---
+++ Source/ImageCapture/Private/Actors/ImageTargetActor.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ImageCapture/Public/Actors/ImageTargetActor.h"
+
+#include "ImageCapture/Public/Components/ImagePointComponent.h"
+
+
+// Sets default values
+AImageTargetActor::AImageTargetActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AImageTargetActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AImageTargetActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+TArray<UImagePointComponent*> AImageTargetActor::GetAllImagePoints() const
+{
+	TArray<UImagePointComponent*> res;
+	GetComponents(UImagePointComponent::StaticClass(), res);
+	return res;
+}
+
+TArray<FVector> AImageTargetActor::GetImagePointsLocations() const
+{
+	TArray<UImagePointComponent*> components = GetAllImagePoints();
+
+	TArray<FVector> res;
+	for (UImagePointComponent* entity : components)
+	{
+		res.Add(entity->GetComponentLocation());
+	}
+	return res;
+}
---
+++ Source/ImageCapture/Public/Actors/ImageTargetActor.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ImageTargetActor.generated.h"
+
+class UImagePointComponent;
+
+UCLASS()
+class IMAGECAPTURE_API AImageTargetActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AImageTargetActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	TArray<UImagePointComponent*> GetAllImagePoints() const;
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetImagePointsLocations() const;
+};
